e40e4c504fa88bc931bd9a81a2a760b3f0a36122
susanalozanoadrio <tuxaymoe@yahoo.es>
2019-07-20 12:45:55 +0200

    conversor de temperatura con validaciones y calculo de conversión terminado

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ class mainApp(Tk):
     entrada = None
     tipoUnidad = None
     
+    __temperaturaAnterior = ""
+    
     def __init__(self):
         Tk.__init__(self)
         self.title("Termometro")
@@ -13,6 +15,7 @@ class mainApp(Tk):
         self.resizable(0,0)
         
         self.temperatura = StringVar(value = "")
+        self.temperatura.trace("w", self.validateTemperature)
         self.tipoUnidad = StringVar(value = "C")
         
         self.createLayout()
@@ -21,13 +24,36 @@ class mainApp(Tk):
         self.entrada = ttk.Entry(self, textvariable = self.temperatura).place(x=10, y=10)
         
         self.lbUnidad = ttk.Label(self, text = 'Grados').place(x=15, y=45)
-        self.rb1= ttk.Radiobutton(self, text = 'Fahrenheit',variable = self.tipoUnidad, value = 'F').place(x=20,y=70)
-        self.rb2 = ttk.Radiobutton(self, text = 'Celsius',variable = self.tipoUnidad, value = 'C').place(x=20,y=90)
+        self.rb1= ttk.Radiobutton(self, text = 'Fahrenheit',variable = self.tipoUnidad, value = 'F', command=self.selected).place(x=20,y=70)
+        self.rb2 = ttk.Radiobutton(self, text = 'Celsius',variable = self.tipoUnidad, value = 'C', command=self.selected).place(x=20,y=90)
         
     def start(self):
         self.mainloop()
         
-        
+    def validateTemperature(self, *args):
+        nuevoValor = self.temperatura.get()
+        print("nuevo valor ", nuevoValor, "vs valorAnterior", self.__temperaturaAnterior)
+        try:
+            float(nuevoValor)
+            self.__temperaturaAnterior = nuevoValor
+            print("fija valor anterior a ", self.__temperaturaAnterior)
+        except:
+            self.temperatura.set(self.__temperaturaAnterior)
+            print("recupera valor anterior ", self.__temperaturaAnterior)
+            
+    def selected(self):
+        resultado = 0
+        toUnidad = self.tipoUnidad.get()
+        grados = float(self.temperatura.get())
+
+        if toUnidad == 'F':
+            resultado = grados * 9/5 + 32
+        elif toUnidad == 'C':
+            resultado = (grados - 32) * 5/9
+        else:
+            resultado = grados
+            
+        self.temperatura.set(resultado)
 if __name__ == "__main__":
     app = mainApp()
     app.start()
